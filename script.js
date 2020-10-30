23ed5c57fd8e9719ff633dccf110157b6456093b
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-29 17:36:04 -0700

    add fs.writeFile

--- script.js
+++ script.js
@@ -10,8 +10,20 @@ fs.readFile('./hello.text', (err, data) => {
 const file = fs.readFileSync('./hello.text')
 console.log(file.toString())
 
-fs.appendFile('./hello.text', ' This is so cool!', err => {
+// APPEND
+// fs.appendFile('./hello.text', ' This is so cool!', err => {
+//     if (err) {
+//         console.log(err)
+//     }
+// })
+
+// WRITE
+fs.writeFile('bye.text', 'Sad to see you go.', err => {
     if (err) {
         console.log(err)
     }
 })
+
+
+
+
