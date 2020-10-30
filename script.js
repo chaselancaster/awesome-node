ea7bb692a8c91b213f58009a0b4b305c531bf73f
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-29 17:27:22 -0700

    add readFileSync

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
-fs.readFile('', (err, data) => {
+fs.readFile('./hello.text', (err, data) => {
     if (err) {
         console.log('error')
     }
-    console.log(data)
+    console.log(data.toString())
 })
+
+fs.readFileSync('./hello.text')
